974981f40e8cf1e940cdce74a8dd69f336bfeefa
Willy <willykipchumba22@gmail.com>
2023-10-02 11:02:27 +0300

    completed assignments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  console.log('After DOM Loaded');
+  console.log(document.querySelector('div'));
+
+  let form = document.querySelector('#create-task-form');
+  form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      buildToDo(e.target.new_task_description.value);
+      form.reset();
+  });
 });
+
+function buildToDo(todo) {
+  let ul = document.createElement('ul');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x';
+  ul.textContent = `${todo} `;
+  ul.appendChild(btn);
+  console.log(ul);
+  document.querySelector('#list').appendChild(ul);
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
+
+  console.log('Before DOM Loads');
+  console.log(document.querySelector('div')); 
